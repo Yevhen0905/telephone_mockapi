08ced813b40af33b506271ff006c619922aaa0fc
Vittcenko <evgenav83@gmail.com>
2024-05-14 13:30:58 +0300

    Initial dist subtree commit

---
+++ assets/ContactCreate-DkotdHby.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./ContactForm-EvgFNe5d.js";import{c as e,a,b as c,o}from"./index-BYtxU9eg.js";const s={class:"contact_create"},_=c("h3",{class:"contact_title"},"Add contact",-1),l={__name:"ContactCreate",setup(r){return(n,d)=>(o(),e("div",s,[_,a(t)]))}};export{l as default};
